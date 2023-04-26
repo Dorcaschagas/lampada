f66c5cb0ccadff8783e55cfd1bfa1afdb9db981e
Dorcas Chagas <dorcaspereira30@gmail.com>
2023-04-26 18:31:03 -0300

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//variaveis
+
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+
+function lampOn(){
+    // alert('teste')
+    lamp.src = './img/ligada.jpg';
+}
+
+function lampOff(){
+    // alert('teste')
+    lamp.src = './img/desligada.jpg';
+}
+
+function lampbroken(){
+    // alert('teste')
+    lamp.src = './img/quebrada.jpg';
+}
+
+//"ouvindo o click"
+turnOn.addEventListener ('click', lampOn); // quando clicar, mudar img
+turnOff.addEventListener ('click', lampOff); // quando clicar, mudar img
+lamp.addEventListener ('mouseover', lampOn); // quando clicar, mudar img
+lamp.addEventListener ('mouseleave', lampOff); // quando clicar, mudar img
+lamp.addEventListener('dblclick', lampbroken);
