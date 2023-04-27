401a1819123ab92a945d5cdf1819c5cf99d0beef
Dorcas Chagas <dorcaspereira30@gmail.com>
2023-04-27 18:22:03 -0300

    actual

--- javascript.js
+++ javascript.js
@@ -3,29 +3,40 @@
 const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById('lamp');
+const botao = document.getElementById('botao')
+
+function OnOff(){
+    if(botao.textContent == 'Ligar'){
+        lampOn();
+        botao.textContent = 'Delisgar'
+    } else {
+        lampOff();
+        botao.textContent = 'Ligar'
+    }
+}
 
 function lampOn(){
-    // alert('teste')
-    lamp.src = './img/ligada.jpg';
+    if( ! islampBroken()){
+         lamp.src = './img/ligada.jpg';
+    }
 }
 
 function lampOff(){
-    // alert('teste')
-    lamp.src = './img/desligada.jpg';
+    if( ! islampBroken()){
+        lamp.src = './img/desligada.jpg';
+    }
 }
 
 function lampbroken(){
-    // alert('teste')
     lamp.src = './img/quebrada.jpg';
 }
 
-// function islampBroken(){
-//     return lamp.src.indexOf ('quebrada') > -1
-// }
+function islampBroken(){
+    return lamp.src.indexOf ('quebrada') > -1
+}
+
 
 //"ouvindo o click"
-turnOn.addEventListener ('click', lampOn); // quando clicar, mudar img
-turnOff.addEventListener ('click', lampOff); // quando clicar, mudar img
-lamp.addEventListener ('mouseover', lampOn); // quando clicar, mudar img
-lamp.addEventListener ('mouseleave', lampOff); // quando clicar, mudar img
+botao.addEventListener('click', OnOff)
+lamp.addEventListener ('mouseover', lampOn);
 lamp.addEventListener('dblclick', lampbroken);
